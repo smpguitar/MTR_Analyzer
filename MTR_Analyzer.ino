eeab5beb8be61819c7518e0e3e125dabd5928019
smpguitar <45099786+smpguitar@users.noreply.github.com>
2022-02-23 21:15:51 -0600

    All Menus Working Correctly
    Still need to add automatic and manual modes for execute
    need to remove unused variables

--- MTR_Analyzer.ino
+++ MTR_Analyzer.ino
@@ -24,7 +24,7 @@ String param = "menu"; //parameter being adjusted
 //array sizes
 int varSize = sizeof var / (sizeof var[0] + 1);
 int modeSize = sizeof mode/ (sizeof mode[0] + 1);
-int bandSize = sizeof band/ (sizeof band[0] + 1);
+int bandSize = (sizeof band - 1)/ (sizeof band[0]);
 int indexSize = sizeof increment / (sizeof increment[0]+1) ;  //number of elements in array
 
 //user selected parameter variables
@@ -78,7 +78,11 @@ if (state == "menu" && param == "menu"){
 lcdMenu();
 } else if (state == "param" && param == "step"){
   setFreqStep();
-  }
+  }else if (state == "param" && param == "band"){
+    setMeters();
+    }else if (state == "param" && param == "mode"){
+    setMode();
+    }
 //si5351.set_freq(Freq, SI5351_CLK0);
 
 }
@@ -89,7 +93,6 @@ int onTurn(EncoderButton& eb){
  lcd.clear();
  index = index + abs(eb.increment()); 
  index = index;
- Serial.println(index);
   } 
 
 void onClick(EncoderButton& eb){
@@ -102,15 +105,35 @@ void onClick(EncoderButton& eb){
       state = "menu";
       param = "menu";
       Serial.println(FreqStep);
+      } else if (button.clickCount() == 1 && index == 1 && state == "menu"){
+    param = "band";
+    lcd.clear();
+    setMeters(); 
       }
+    else if (button.clickCount() == 1 && state == "param" && param == "band"){
+      bandSelected = band[index];
+      state = "menu";
+      param = "menu";
+     Serial.println(bandSelected);
+ } else if (button.clickCount() == 1 && index == 0 && state == "menu"){
+    param = "mode";
+    lcd.clear();
+    setMode(); 
+      }
+    else if (button.clickCount() == 1 && state == "param" && param == "mode"){
+      modeSelected = mode[index];
+      state = "menu";
+      param = "menu";
+      lcd.clear();
+     Serial.println(modeSelected);
  }
+}
 
 
 // other Functions
 void setFreqStep(){
   state = "param";
-  if (index >  indexSize) index = 0;
-  if (index < -indexSize) index = 0;
+  if (index > indexSize) index = 0;
   lcd.setCursor(0,0);
   lcd.print("Frequency Select");  
   lcd.setCursor(7,1);
@@ -118,9 +141,28 @@ void setFreqStep(){
   Serial.println(state);
   }
 
+void setMeters(){
+  state = "param";
+  if (index > bandSize) index = 0;
+  lcd.setCursor(2,0);
+  lcd.print("Band Select");  
+  lcd.setCursor(7,1);
+  lcd.print(band[index]); 
+  Serial.println(bandSize);
+  }
+
+  void setMode(){
+  state = "param";
+  if (index > modeSize) index = 0;
+  lcd.setCursor(2,0);
+  lcd.print("Mode Select");  
+  lcd.setCursor(7,1);
+  lcd.print(mode[index]); 
+  Serial.println(modeSize);
+  }
+
 void lcdMenu(){
   if (index >  varSize) index = 0;
-  if (index < -varSize) index = 0;
   lcd.setCursor(0,0);
   lcd.print("Select Parameter");
   lcd.setCursor(6,1);
